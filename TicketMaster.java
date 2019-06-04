8a5630d0173c588fffce5608a9945fc934229c06
dannyaudi <50224134+dannyaudi@users.noreply.github.com>
2019-06-04 12:41:08 -0400

    Add files via upload

--- TicketMaster.java
+++ TicketMaster.java
@@ -4,16 +4,25 @@ public class TicketMaster
 {
     public static void main (String[] args)
     {
-	int userchoice;
-
 	System.out.println ("Welcome to the online TicketMaster");
 
+	// temporary
 	NewStadium a = new NewStadium ("Concert", "01/01/2000");
-	
-	ArrayList events = new ArrayList();
 
-	events.add(a);
+	// list of NewStadiums, contains all the events created
+	ArrayList events = new ArrayList ();
 	
+	// temporary
+	events.add (a);
+
+	// List of strings containing info on tickets, according to their confirmation number
+	// example, index 0 is for confirmation number 1001
+	ArrayList confirmation = new ArrayList ();  
+    
+	// this field is used in the numbering of confirmation numbers and is incremented as tickets are purchased
+	int codeNumber = 1000;
+
+	int userchoice;
 	do
 	{
 	    System.out.println ("\nPlease enter which option you wish to use\n");
@@ -31,7 +40,7 @@ public class TicketMaster
 
 		// here we will select an event
 
-		Utils.Purchase (a);
+		Utils.Purchase (a, confirmation);
 
 		System.out.println ("Forwarding to process your payment");
 		System.out.println ("...");
@@ -48,8 +57,9 @@ public class TicketMaster
 		{
 		    Thread.currentThread ().interrupt ();
 		}
-
-		System.out.println ("Transaction Complete. Your confirmation code is " /*CONFIRMATION*/+ ". Returning to main menu.");
+	       
+		codeNumber++;
+		System.out.println ("Transaction Complete. Your confirmation code is " + codeNumber + ". Returning to main menu.");
 
 		try
 		{
@@ -64,24 +74,52 @@ public class TicketMaster
 	    else if (userchoice == 2)
 	    {
 		System.out.println ("Check Ticket Status");
+		System.out.print ("\nPlease enter your confirmation code: ");
+		
+		int codeInput = TextIO.getlnInt ();
+		
+		while (codeInput <= 1000 || codeInput - 1001 >= confirmation.size() || codeInput < 0)
+		{        
+		    //System.out.println("TEST2   " + (codeInput - 1001) + "    " + (codeInput - 1001 >= confirmation.size ()));
+		    if (codeInput < 1000 || codeInput - 1001 >= confirmation.size () || codeInput - 1001 < 0)
+			System.out.println ("That confirmation code does not exist. Please try again or enter 0 to exit.");
+		    
+		    codeInput = TextIO.getlnInt ();
+		    
+		    if(codeInput == 0)
+			break;                        
+		}                 
+		
+		if(codeInput == 0)
+		    System.out.println("Returning to main menu.");
+		else
+		    System.out.println ("Your ticket is for: " + confirmation.get (codeNumber - 1001));
 	    }
 
 	    else if (userchoice == 3)
 	    {
 		System.out.println ("Check Availability");
-		
-		for(int i = 0; i < events.size(); i++)
+
+		for (int i = 0 ; i < events.size () ; i++)
 		{
-		    System.out.println(events.get(i).printInfo(events.get(i)));
-		    // Need to declare Arraylist of only type NewStadium, or change methods
+		    // Need to declare Arraylist of only type NewStadium, or change method parameters
+		    //System.out.println(events.get(i).printInfo(events.get(i)));
 		    //NewStadium temp = events.get(i);
-		    //System.out.println(temp.toString() + " | " + temp.sumReserved(temp)); 
+		    //System.out.println(temp.toString() + " | " + temp.sumReserved(temp));
 		}
 	    }
 
 	    else if (userchoice == 4)
 	    {
-		System.out.println ("Operator Menu"); 
+		System.out.println ("Operator Menu");
+		
+		System.out.println("Enter the option you wish to use\n");
+		System.out.println("1) Create new event");
+		System.out.println("2) Rename an event");
+		System.out.println("3) Change the date of an event");
+		System.out.println("4) Delete an event");
+		System.out.println("5) Check log details");
+		System.out.println("Enter 0 to exit");
 	    }
 
 	    else if (userchoice == 5)
@@ -94,7 +132,7 @@ public class TicketMaster
     }
 
 
-    
+
 
 
 }
