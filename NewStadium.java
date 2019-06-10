4406d4cf1c7fe752db9828fcd233c281577a60bb
dannyaudi <50224134+dannyaudi@users.noreply.github.com>
2019-06-10 12:55:06 -0400

    Final version with comments

--- NewStadium.java
+++ NewStadium.java
@@ -1,3 +1,6 @@
+// The NewStadium object is used to create events within TicketMaster
+// Each NewStadium is comprised of a seating map, event name, and event date
+
 public class NewStadium
 {
     private String[] [] stadium;
@@ -8,41 +11,42 @@ public class NewStadium
     {
 	name = n;
 	date = d;
-	
-	stadium = new String[][]{
-		{"0", "0", "0", "0", "0", "0"},
-		{"0", "0", "0", "0", "0", "0"},
-		{"0", "0", "X", "X", "0", "0"},
-		{"0", "0", "X", "X", "0", "0"},
-		{"0", "0", "0", "0", "0", "0"},
-		{"0", "0", "0", "0", "0", "0"},
-	    };
+
+	stadium = new String[] []
+	{
+	    {"0", "0", "0", "0", "0", "0"},
+	    {"0", "0", "0", "0", "0", "0"},
+	    {"0", "0", "X", "X", "0", "0"},
+	    {"0", "0", "X", "X", "0", "0"},
+	    {"0", "0", "0", "0", "0", "0"},
+	    {"0", "0", "0", "0", "0", "0"},
+	};
     }
 
-    // Prints the stadium with rows and columns labelled
+    // Prints the stadium map with rows and columns labelled
     public void print (NewStadium a)
     {
-
 	String[] [] s = a.getStadium ();
 
-	System.out.println("    0 1 2 3 4 5");
-	System.out.println("   ------------");
-	
+	System.out.println ("    0 1 2 3 4 5");
+	System.out.println ("   ------------");
+
 	for (int i = 0 ; i < s.length ; i++)
 	{
-	    System.out.print(i + " | ");
+	    System.out.print (i + " | ");
 	    for (int j = 0 ; j < s [0].length ; j++)
 	    {
 		System.out.print (s [i] [j] + " ");
 	    }
-	    System.out.println ();            
+	    System.out.println ();
 	}
     }
-    
+
+
     // Returns the name and date of the event formatted neatly
-    public String printInfo(NewStadium a)
+    public String printInfo (NewStadium a)
     {
-	return a.getName() + " on " + a.getDate(); 
+	return a.getName () + " on " + a.getDate ();
     }
 
     // returns the stadium as an array of Strings
@@ -50,27 +54,32 @@ public class NewStadium
     {
 	return stadium;
     }
-    
-    public String getName()
+
+    // Returns the name of an event as a String
+    public String getName ()
     {
 	return name;
     }
-    
-    public String getDate()
+
+    // Returns the date of an event as a String
+    public String getDate ()
     {
 	return date;
-	}
-	
-	public void setName(String n)
-	{
-		name = n;
-	}
+    }
 
-	public void setDate(String d)
-	{
-		date = d;
-	}
-    
+    // Sets a new name for an event
+    public void setName (String n)
+    {
+	name = n;
+    }
+
+    // Sets a new date for an event
+    public void setDate (String d)
+    {
+	date = d;
+    }
+
+
     // Returns the number of seats reserved in a stadium
     public int sumReserved (NewStadium a)
     {
@@ -82,29 +91,31 @@ public class NewStadium
 	{
 	    for (int j = 0 ; j < s [0].length ; j++)
 	    {
-		if (s [i] [j] == "R")
+		if (s [i] [j] == "R")   // Counts each seat with a reserved value of "R"
 		    counter++;
 	    }
 	}
 	return counter;
     }
-    
+
+
     // Reserves a seat by rewriting spot as "R"
-    public String[][] reserveSeat(NewStadium a, int row, int col)
+    public String[] [] reserveSeat (NewStadium a, int row, int col)
     {
 	String[] [] s = a.getStadium ();
-    
-	s[row][col] = "R";
-	
+
+	s [row] [col] = "R";
+
 	return s;
     }
-    
-    // Checks if a seat is available to purchase
-    public boolean checkReserved(NewStadium a, int row, int col)
+
+
+    // Checks if a seat is available to purchase, i.e. is not "R" or "X"
+    public boolean checkReserved (NewStadium a, int row, int col)
     {
 	String[] [] s = a.getStadium ();
-	
-	if(s[row][col] == "R" || s[row][col] == "X")
+
+	if (s [row] [col] == "R" || s [row] [col] == "X")
 	    return true;
 	else
 	    return false;
